c7b8982d1cfd6d8aa294ccbaa066a10306badad6
Magnar Sveen <magnars@gmail.com>
2013-04-20 17:29:55 +0200

    Release 1.4.0

--- s.el
+++ s.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 Magnar Sveen
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
-;; Version: 1.3.1
+;; Version: 1.4.0
 ;; Keywords: strings
 
 ;; This program is free software; you can redistribute it and/or modify
