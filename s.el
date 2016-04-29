2c461d5ef04b520134d167aafee903d1ffaacdaa
lujun9972 <lujun9972@gmail.com>
2016-04-29 15:10:24 +0800

    when origin 's--aget' return nil. It should keep nil

--- s.el
+++ s.el
@@ -333,7 +333,9 @@ This is a simple wrapper around the built-in `string-match-p'."
   (replace-regexp-in-string (regexp-quote old) new s t t))
 
 (defun s--aget (alist key)
-  (format "%s" (cdr (assoc-string key alist))))
+  (let ((result (cdr (assoc key alist))))
+    (when result
+      (format "%s" result))))
 
 (defun s-replace-all (replacements s)
   "REPLACEMENTS is a list of cons-cells. Each `car` is replaced with `cdr` in S."
