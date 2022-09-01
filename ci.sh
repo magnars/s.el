e1d985e0aaf027ed520648902b0aa8d000a0ed43
Jason Milkins <jasonm23@users.noreply.github.com>
2022-09-01 13:15:27 +0700

    Set EMACS to which emacs in ci.sh
    Remove set-default shell function in ci.sh

--- ci.sh
+++ ci.sh
@@ -2,15 +2,7 @@
 
 cd "$(dirname "$0")"
 
-set_default () {
-  eval "
-if [ -z \$$1 ]; then
-  $1=$2
-fi
-"
-}
-
-set_default EMACS "$(which emacs)"
+EMACS="$(which emacs)"
 
 echo "*** Emacs version ***"
 echo "EMACS =" $(which $EMACS)
